1b4c332a21253926dad5db292ff3a045f2cbe692
Kenneth Thompson <kenneth.thompson@ou.edu>
2024-02-17 10:05:57 -0600

    Changed background color

--- GameManager.java
+++ GameManager.java
@@ -8,14 +8,20 @@ public class GameManager extends JFrame{
     
     public GameManager(){
         JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension screenSize = toolkit.getScreenSize();
 
-        // Set window title and visiblity
+        Color crimson = new Color(153, 0, 0);
+
+        // Set window title, visiblity, and size
         frame.setTitle("OU Trivia");
         frame.setVisible(true);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.setSize(screenSize.width, screenSize.height);
         pack();
+
+        // Set background color
+        frame.getContentPane().setBackground(crimson);
     }
 }
